d2f2a5715b6105b4bf5f29590650d8ea4907e68e
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-13 18:19:16 +0100

    Numeri random per la carrozza e il treno
    - Generatore casuale numeroCarrozza tra 1 e 9;
    - Generatore casuale codiceTreno tra 90.000 e 100.000 escluso;

--- script.js
+++ script.js
@@ -6,3 +6,11 @@
 // Prezzo calcolato
 // Categoria selezionata dall'utente (offerta)
 // Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
+
+// Numero casuale codice treno 90k - 100k
+var codiceTreno = Math.floor(Math.random() * (100000 - 90000 + 1)) + 90000;
+console.log("codiceTreno " + codiceTreno);
+
+// Numero casuale numero carrozza 1 - 9
+var numeroCarrozza = Math.ceil(Math.random()*9);
+console.log("Numero casuale carrozza " + numeroCarrozza);
