2e5981ffa5ebbf0b9d26c216db52f7d8c45c473f
Giuseppe <giuseppe.dagostino.d@gmail.com>
2020-11-13 16:45:52 +0100

    Avvio progetto

---
+++ script.js
@@ -0,0 +1,8 @@
+// TRACCIA
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza (numero casuale tra 1 e 9)
+// Prezzo calcolato
+// Categoria selezionata dall'utente (offerta)
+// Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
